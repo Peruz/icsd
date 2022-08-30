4e60fda2311335b367b6ae5aa9cc2b3ecdfc79c2
BenjMy <36472081+BenjMy@users.noreply.github.com>
2022-08-30 21:01:27 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_logo = 'images/logo.png'
-html_theme = 'sphinx_rtd_theme' #sphinx_book_theme
+html_theme = 'sphinx_book_theme' #sphinx_book_theme
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
